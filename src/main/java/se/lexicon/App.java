ba0bdadecad3fdf25e005fe642ce117bb4fd802d
Erik Svensson <eosvensson@gmail.com>
2020-04-21 12:04:05 +0200

    Lecture finished 2020-04-21

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,13 +1,25 @@
 package se.lexicon;
 
+import se.lexicon.utility.StringArrayUtil;
+
+import java.util.Arrays;
+
 /**
- * Hello world!
+ * @author Erik Svensson
+ * 1. Create exapanding array
+ * 2. Sort array
+ * 3. toString representation
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        String[] names = new String[0];
+        names = StringArrayUtil.add(names, "Mattias Andersson");
+        names = StringArrayUtil.add(names, "Michelle Johansson");
+        names = StringArrayUtil.addAndSort(names, "Ameer Rasheed");
+
+        System.out.println(Arrays.toString(names));
     }
 }
